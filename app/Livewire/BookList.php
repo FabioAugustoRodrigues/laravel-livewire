8a03cece53dc38c85972c5a19263f50a7fe3cd74
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2026-05-19 18:33:08 -0300

    add delete button

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -9,6 +9,11 @@ class BookList extends Component
 {
     public $name = 'Mario';
 
+    public function delete(Book $book)
+    {
+        $book->delete();
+    }
+    
     public function render()
     {
         return view('livewire.book-list',[
